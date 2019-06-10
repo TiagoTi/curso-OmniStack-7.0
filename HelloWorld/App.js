f5acfa99166d64fad276aebb6b3fc2e8126351ca
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-10 18:06:00 -0300

    add the fist image component to view.

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 export default class HelloWorldApp extends Component {
 
 
   render() {
+    let pictureURI = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }
     return (
       <View style={ styles.container }>
         <Text style={ styles.textDefault }>Hello, world!</Text>
+        <Image source={ pictureURI } style={{width: 193, height: 110}} />
       </View>
     );
   }
