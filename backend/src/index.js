536c0c3e5dc6bc1742b4f5cba9eae6c5cae8d407
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-11 16:44:16 -0300

    add database config to project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
+
+const process = require('process')
+
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect(
+    `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`,
+    {useNewUrlParser: true}
+);
 
 app.get('/', (req,res)=>{
     return res.send(`Hello World: ${req.query.name}`)
