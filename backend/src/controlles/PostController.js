5420751eb3c98ffe1aa2ad2ca35c9d7ceefa1a3b
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-11 18:16:20 -0300

    save post on mongo

--- backend/src/controlles/PostController.js
+++ backend/src/controlles/PostController.js
@@ -5,7 +5,17 @@ module.exports = {
 
 
     async store(req, res){
-        return res.json({ok:true})
+        const {author, place, description, hastags} = req.body
+        const {filename: image} = req.file
+
+        const  post = await Post.create({
+            author,
+            place,
+            description,
+            hastags,
+            image
+        })
+        return res.json(post)
     }
 
 
