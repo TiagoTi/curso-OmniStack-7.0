50a2413e00049e904f9d5311eb6f2913a495c08f
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-11 17:39:29 -0300

    conect call post to controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,21 @@ const express = require('express')
 
 const routes = express.Router()
 
+const PostController = require('./controlles/PostController')
+
 
 routes.get('/', (req,res)=>{
     return res.send(`Hello World: ${req.query.name}`)
+})
+
+
+routes.post('/posts', (req,res)=>{
+    console.log(req.params)
+    console.log(req.ip)
+    req.on('data', (data)=>{
+        console.log(data.toString());
+    })
+    PostController.store(req, res)
 })
 
 
