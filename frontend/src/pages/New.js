b29a51e30f15a715a9e408eb7604cdc2b7beb536
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 10:06:24 -0300

    init page to create a new post

---
+++ frontend/src/pages/New.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class New extends Component {
+
+
+    render(){
+        return(
+            <div className="component">
+                <h1>New</h1>
+            </div>
+        )
+    }
+
+
+}
+
+
+export default New
