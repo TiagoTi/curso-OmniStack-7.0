a7046cc0ec47707cdfb04fdb78abc0ac9a73b067
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 20:05:12 -0300

    conect state with html form

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -5,15 +5,40 @@ import './New.css'
 class New extends Component {
 
 
+    state  = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hastags: ''
+    }
+
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+
+    handleImageChange = e => {
+        this.setState({image: e.target.files[0]})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log('Form')
+        console.log(this.state)
+    }
+
+
     render(){
         return(
             <div className="component">
-                <form id="new-post">
-                    <input type="file"/>
-                    <input type="text" name="author" placeholder="Autor do post"/>
-                    <input type="text" name="place" placeholder="Local do post"/>
-                    <input type="text" name="description" placeholder="Descrição do post"/>
-                    <input type="text" name="hastags" placeholder="Tags do post"/>
+                <form id="new-post" onSubmit={this.handleSubmit}>
+                    <input type="file" onChange={this.handleImageChange}/>
+                    <input type="text" onChange={this.handleChange} value={this.state.author} name="author" placeholder="Autor do post"/>
+                    <input type="text" onChange={this.handleChange} value={this.state.place} name="place" placeholder="Local do post"/>
+                    <input type="text" onChange={this.handleChange} value={this.state.description} name="description" placeholder="Descrição do post"/>
+                    <input type="text" onChange={this.handleChange} value={this.state.hastags} name="hastags" placeholder="Tags do post"/>
                     <button type="submit">Enviar</button>
                 </form>
             </div>
