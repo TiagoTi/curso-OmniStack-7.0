f2c79db6085fecda0ebf7a4f8b8e337a06f1ef33
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 18:44:35 -0300

    create a form to get data

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 
+import './New.css'
+
 class New extends Component {
 
 
     render(){
         return(
             <div className="component">
-                <h1>New</h1>
+                <form id="new-post">
+                    <input type="file"/>
+                    <input type="text" name="author" placeholder="Autor do post"/>
+                    <input type="text" name="place" placeholder="Local do post"/>
+                    <input type="text" name="description" placeholder="Descrição do post"/>
+                    <input type="text" name="hastags" placeholder="Tags do post"/>
+                    <button type="submit">Enviar</button>
+                </form>
             </div>
         )
     }
