fdb161ac850e3993b0a35ee8110321169ac99ca0
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 18:25:42 -0300

    render each post geted from api

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,63 +1,54 @@
 import React, { Component } from 'react'
 
 import './Feed.css'
+
 import more from '../assets/more.svg'
 import like from '../assets/like.svg'
 import comment from '../assets/comment.svg'
 import send from '../assets/send.svg'
+import api from '../services/api'
 
 class Feed extends Component {
+
+
+    state = {
+        feed: []
+    }
+
+
+    async componentDidMount(){
+        const response = await api.get('posts');
+        this.setState({feed: response.data})
+    }
 
 
     render() {
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Tiago Domingos Cipriano</span>
-                            <span className="place">Franca SP</span>
-                        </div>
+            {this.state.feed.map(post=>(
+                <article key={post._id}>
+                <header>
+                    <div className="user-info">
+                        <span>{post.author}</span>
+                        <span className="place">{post.place}</span>
+                    </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+                    <img src={more} alt="Mais" />
+                </header>
 
-                    <img src="http://127.0.0.1:3333/files/Screenshot from 2019-06-10 13-04-04.jpg" alt="" />
+                <img src={`http://127.0.0.1:3333/files/${post.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Dar like" />
-                            <img src={comment} alt="Comentar" />
-                            <img src={send} alt="enviar" />
-                        </div>
-                        <strong>900 curtida</strong>
-                        <p>Site toper D+<span>#react #ti2</span></p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Tiago Domingos Cipriano</span>
-                            <span className="place">Franca SP</span>
-                        </div>
-
-                        <img src={more} alt="Mais" />
-                    </header>
-
-                    <img src="http://127.0.0.1:3333/files/Screenshot from 2019-06-10 13-04-04.jpg" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Dar like" />
-                            <img src={comment} alt="Comentar" />
-                            <img src={send} alt="enviar" />
-                        </div>
-                        <strong>900 curtida</strong>
-                        <p>Site toper D+<span>#react #ti2</span></p>
-                    </footer>
-                </article>
-                <iframe src="https://docs.emmet.io/abbreviations/syntax/" frameborder="0" width="890" height="10100"></iframe>
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Dar like" />
+                        <img src={comment} alt="Comentar" />
+                        <img src={send} alt="enviar" />
+                    </div>
+                    <strong>{post.likes} curtida</strong>
+                    <p>{post.description}<span>{post.hastags}</span></p>
+                </footer>
+            </article>
+            ))}
             </section>
         )
     }
