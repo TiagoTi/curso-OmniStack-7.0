f0d5421a7100334d5a73f8581ab94aafb4b4ea0a
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 12:56:00 -0300

    add Link to / and /news

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 import './Header.css'
 import logo from '../assets/logo.svg'
@@ -8,8 +9,12 @@ export default function Header() {
   return (
     <header id="main-header">
         <div className="header-content">
+            <Link to='/'>
             <img src={logo} alt="InstaRocket"/>
+            </Link>
+            <Link to='/new'>
             <img src={camera} alt="Enviar Publicação"/>
+            </Link>
         </div>
     </header>
   );
