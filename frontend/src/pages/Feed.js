a9e64cd5583b9c4dc7883a7ebada626109253b59
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-12 10:06:10 -0300

    init from page of posts

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Feed extends Component {
+
+
+    render(){
+        return(
+            <div className="component">
+                <h1>Feed</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default Feed
