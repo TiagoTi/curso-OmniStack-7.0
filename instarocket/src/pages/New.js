ecd705532fa0745b9fbef25f3b504f8e46dd8974
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 13:49:31 -0300

    init page New

---
+++ instarocket/src/pages/New.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+export default class New extends Component {
+  render() {
+    return (
+        <View >
+        <Text >Welcome to React New!</Text>
+      </View>
+    )
+  }
+}
