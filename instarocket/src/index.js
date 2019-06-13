2116e052424edd7c10bf68344f8cf40031783ffb
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 13:57:47 -0300

    remove unused code

--- instarocket/src/index.js
+++ instarocket/src/index.js
@@ -1,27 +1,7 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
+import Routes from './routes'
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View>
-    );
-  }
+export default function App(){
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
