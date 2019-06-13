8e6a3ea40b0c89b1de32dad4510b35a0b92ba939
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 15:40:01 -0300

    import datas

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -7,7 +7,6 @@ import like from '../assets/like.png'
 import comment from '../assets/comment.png'
 import send from '../assets/send.png'
 
-
 import api  from '../services/api'
 
 
@@ -48,12 +47,36 @@ async componentDidMount(){
           renderItem={({item})=>(
             
             <View style={styles.feedItem}>
+              <View style={styles.feedItemHeader}>
               <View style={styles.userInfo}>
                 <Text style={styles.name}>{item.author}</Text>
                 <Text style={styles.place}>{item.place}</Text>
               </View>
-            </View>
 
+              <Image source={more} />
+              </View>
+
+              <Image source={{ uri: `http://172.16.0.30:3333/files/${item.image}` }} style={styles.feedImage} />
+              
+              <View style={styles.feedItemFooter}>
+                <View style={styles.actions}>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={like} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={comment} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => {}}>
+                    <Image source={send} />
+                  </TouchableOpacity>
+                </View>
+                </View>
+                <Text style={styles.likes}>{item.likes} curtidas</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.hashtags}>{item.hastags}</Text>
+            </View>
           )}
         />
       </View>
