fe1a96599f7c7d2e8740d899e0d45017b80aaf1a
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 14:39:17 -0300

    add camera button on header

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import camera from '../assets/camera.png'
 
 
 export default class Feed extends Component {
+  static navigationOptions = {
+    headerRight: (
+      <TouchableOpacity onPress={()=>{}} style={{marginRight: 20}}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
         <View >
-        <Text >Welcome to React Feed!</Text>
+        <Text >Welcome to React Feed!!</Text>
       </View>
     )
   }
