682735c598327ab0f67f2813a654e9d3fab19b40
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 13:49:52 -0300

    create stack navigation

---
+++ instarocket/src/routes.js
@@ -0,0 +1,10 @@
+import {createAppContainer, createStackNavigator} from 'react-navigation'
+
+import Feed from './pages/Feed'
+import New  from './pages/New'
+
+export default createAppContainer(
+    createStackNavigator({
+        Feed, New
+    })
+)
