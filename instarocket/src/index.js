3279ab1a968b95c2003ce5e1053376d15f9ff486
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-13 16:35:49 -0300

    ignore web socket erro warning

--- instarocket/src/index.js
+++ instarocket/src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from 'react'
+import {YellowBox} from 'react-native'
 
 import Routes from './routes'
 
+
+YellowBox.ignoreWarnings([
+  'WebSocket'
+])
 export default function App(){
   return <Routes />
 }
