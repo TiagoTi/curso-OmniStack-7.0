acba0118b2dc50782950f0dc25a4049e24b98918
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 13:21:49 -0300

    fix eslinter

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -1,13 +1,13 @@
-const multer = require('multer')
+const multer = require('multer');
 
-const path = require('path')
+const path = require('path');
 
 
 module.exports = {
-    storage: new multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'uploads'),
-        filename: (req, file, cb)=>{
-            cb(null, file.originalname)
-        }
-    })
-}
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+};
