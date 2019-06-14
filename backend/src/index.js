c65abc2929d2c5c51298596a4528371bc8e792cf
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 13:25:12 -0300

    fix: eslinter

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,45 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const path = require('path')
+const path = require('path');
 
-const process = require('process')
+const process = require('process');
 
-const express = require('express')
+const express = require('express');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const cors = require('cors')
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-const server = require('http').Server(app)
+const server = require('http').Server(app);
 
-const io = require('socket.io')(server)
+const io = require('socket.io')(server);
 
-app.use(cors())
+app.use(cors());
 
 mongoose.connect(
-    `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`,
-    {useNewUrlParser: true}
+  `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`,
+  { useNewUrlParser: true },
 );
 
-app.use((req, res, next)=>{
-    req.io = io
-    next()
-})
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 
 app.use(
-    '/files',
-    express.static(
-        path.resolve(
-            __dirname, '..','uploads', 'resized'
-        )
-    )
-)
+  '/files',
+  express.static(
+    path.resolve(
+      __dirname, '..', 'uploads', 'resized',
+    ),
+  ),
+);
 
 
-app.use(require('./routes'))
+app.use(require('./routes'));
 
 
-server.listen(process.env.APP_SERVER_PORT)
+server.listen(process.env.APP_SERVER_PORT);
