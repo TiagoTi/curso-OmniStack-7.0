a0841664e85af48c3e268981fa00e63f9b12f1e8
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 13:22:54 -0300

    fix: eslinter

--- backend/src/controlles/PostController.js
+++ backend/src/controlles/PostController.js
@@ -1,43 +1,45 @@
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-const sharp = require('sharp')
+const sharp = require('sharp');
 
-const Post = require('../models/Post')
+const Post = require('../models/Post');
 
 
 module.exports = {
 
 
-    async store(req, res){
-        const {author, place, description, hastags} = req.body
-        const {filename: image} = req.file
-        const [name] = image.split(".")
-        const fileName = `${name}.jpg`
+  async store(req, res) {
+    const {
+      author, place, description, hastags,
+    } = req.body;
+    const { filename: image } = req.file;
+    const [name] = image.split('.');
+    const fileName = `${name}.jpg`;
 
-        await sharp(req.file.path)
-            .resize(500)
-            .jpeg({quality: 70})
-            .toFile(
-                path.resolve(req.file.destination, 'resized', fileName)
-            )
-        
-        fs.unlinkSync(req.file.path)
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName),
+      );
 
-        const  post = await Post.create({
-            author,
-            place,
-            description,
-            hastags,
-            image: fileName
-        })
-        req.io.emit('post', post)
-        return res.json(post)
-    },
+    fs.unlinkSync(req.file.path);
+
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hastags,
+      image: fileName,
+    });
+    req.io.emit('post', post);
+    return res.json(post);
+  },
 
 
-    async index(req, res){
-        const posts = await Post.find().sort('-createdAt')
-        return res.json(posts)
-    }
-}
+  async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
+    return res.json(posts);
+  },
+};
